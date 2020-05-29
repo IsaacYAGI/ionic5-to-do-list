8a1966e9985d7f0420baaa114de826f8b7080271
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-29 16:10:48 -0500

    Se navega a la pagina Agregar

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
 import { Lista } from 'src/app/models/lista.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -12,9 +13,15 @@ export class Tab1Page {
   listadoTareas: Lista[] = [];
 
   constructor(
-    public deseosService: DeseosService
+    public deseosService: DeseosService,
+    private router: Router
   ) { 
     this.listadoTareas = deseosService.listas;
+  }
+
+  agregarLista(){
+    console.log("Navigating...");
+    this.router.navigateByUrl("/tabs/tab1/agregar");
   }
 
 }
