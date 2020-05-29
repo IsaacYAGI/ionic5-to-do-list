5d9266ca3ca431eb1d0c1fbd1a17158b823f5401
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-29 16:37:06 -0500

    Agrega alertcontroller

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
 import { Lista } from 'src/app/models/lista.model';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -14,14 +15,47 @@ export class Tab1Page {
 
   constructor(
     public deseosService: DeseosService,
-    private router: Router
+    private router: Router,
+    private alertCtrl: AlertController
   ) { 
     this.listadoTareas = deseosService.listas;
   }
 
-  agregarLista(){
-    console.log("Navigating...");
-    this.router.navigateByUrl("/tabs/tab1/agregar");
+  async agregarLista(){
+    // console.log("Navigating...");
+    // this.router.navigateByUrl("/tabs/tab1/agregar");
+    
+      const alert = await this.alertCtrl.create({
+        header: 'Nueva lista',
+        inputs:[
+          {
+            name:'titulo',
+            type:'text',
+            placeholder:'Nombre de la lista'
+          }
+        ],
+        buttons: [
+          {
+            text:'Cancelar',
+            role:'cancel',
+            handler: () => console.log('Cancelar')
+
+          },
+          {
+            text:'Crear',
+            handler: (data: string) => {
+              if (data.length === 0) return;
+              
+              return;
+            }
+
+          },
+
+        ]
+      });
+  
+      alert.present();
+    
   }
 
 }
